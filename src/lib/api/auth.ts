78ca59f183b46694e611d26f55f8f57c92d141ae
nearHaan <farhaan.nizam675@gmail.com>
2026-02-27 03:27:12 +0530

    add auth user | change fetch to ky

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,22 +1,55 @@
+import { goto } from '$app/navigation';
 import { PUBLIC_API_BASE_URL } from '$env/static/public';
+import { api } from '$lib/api';
+import { authInfo } from '$lib/global/auth.svelte';
+import type { ApiResponse, AuthUser } from '$lib/types';
+
+export async function authUser() {
+	const accessToken = localStorage.getItem('accessToken');
+	if (typeof accessToken !== 'string' || accessToken.trim().length === 0) {
+		logout();
+	}
+	const res = await api
+		.post('auth/me', { headers: { Authorization: `Bearer ${accessToken}` } })
+		.json<ApiResponse<AuthUser>>();
+	if (res.success) {
+		authInfo.user = res.data.user;
+		authInfo.permissions = res.data.permissions;
+	} else {
+		if (res.code === 401) {
+			const refreshRes = await api
+				.post('auth/refresh', { credentials: 'include' })
+				.json<ApiResponse<AuthUser>>();
+			if (refreshRes.success) {
+				authInfo.user = refreshRes.data.user;
+				authInfo.permissions = refreshRes.data.permissions;
+			} else {
+				logout();
+				return;
+			}
+		} else {
+			throw new Error(res.message);
+		}
+	}
+}
+
+export async function logout() {
+	localStorage.clear(); // TODO: rethink - .clear() or removeItem()
+	await api.post('auth/logout');
+	goto('/login');
+}
 
 export async function loginUser(email: string, password: string) {
-	const res = await fetch(`${PUBLIC_API_BASE_URL}/auth/login`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ email, password })
-	});
+	const res = await api.post('auth/login', { json: { email, password } }).json<
+		ApiResponse<{
+			accessToken: string;
+		}>
+	>();
 
-	if (!res.ok) {
-		const error = await res.json().catch(() => ({}));
-		console.error(error);
-		throw new Error(error.message);
+	if (res.success) {
+		localStorage.setItem('token', res.data.accessToken);
+	} else {
+		throw new Error(res.message);
 	}
-
-	const data = await res.json();
-	localStorage.setItem('token', data.accessToken);
-
 	return true;
 }
