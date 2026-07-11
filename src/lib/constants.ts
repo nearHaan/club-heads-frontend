636bb4e1f49224c6bf1e1d7949c57886a32b0388
Adithyan19 <adithyan.m.c.2004@gmail.com>
2026-07-11 16:14:53 +0530

    Implement TreeTable component and integrate it into the organizations page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -34,8 +34,7 @@ export const ROUTE_PERMISSIONS: Record<string, PermissionCode[]> = {
 		'event:view_all_non_draft',
 		'event:view_own'
 	],
-	'/venues': ['venue:add_member', 'venue:create', 'venue:modify_facilities'],
-	'/organizations': ['organization:add_member', 'organization:create']
+	'/venues': ['venue:add_member', 'venue:create', 'venue:modify_facilities']
 } as const;
 
 export const eventStatusColors: Record<Event['status'], string> = {
